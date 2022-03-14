000284d77722c8326a22f3aa2604ffaacf7c8768
autohe <1518585346@qq.com>
2022-03-14 16:19:07 +0800

    增加了网络连接请求工具

---
+++ lib/util/net/network_util.dart
@@ -0,0 +1,59 @@
+import 'package:dio/dio.dart';
+import 'package:frontend/util/debug_print.dart';
+import 'package:frontend/util/net/response_interceptors.dart';
+import 'package:frontend/util/net/result_data.dart';
+
+class HttpManager {
+  static final HttpManager _instance = HttpManager._internal();
+  late Dio? _dio;
+
+  HttpManager._internal() {
+    if (_dio == null) {
+      _dio = Dio(
+          BaseOptions(connectTimeout: 15000)
+      );
+      // _dio!.interceptors.add(DioLogInterceptor());
+      _dio!.interceptors.add(ResponseInterceptors());
+    }
+  }
+
+  factory HttpManager() => _instance;
+
+  Future _handle({required response}) async {
+    Response? myResponse;
+    try {
+      myResponse = response;
+    } on DioError catch (e) {
+      debugPrint(e);
+    }
+
+    if (myResponse!.data is DioError) {
+    }
+
+    return myResponse.data;
+  }
+
+  Future get(path, {args}) async {
+    return _handle(
+        response: await _dio!.get(path, queryParameters: args)
+    );
+  }
+
+  Future post(path, {args}) async {
+    return _handle(
+        response: await _dio!.post(path, queryParameters: args)
+    );
+  }
+
+  Future delete(path, {args}) async {
+    return _handle(
+        response: await _dio!.delete(path, queryParameters: args)
+    );
+  }
+
+  Future put(path, {args}) async {
+    return _handle(
+        response: await _dio!.put(path, queryParameters: args)
+    );
+  }
+}
---
+++ lib/util/net/result_data.dart
@@ -0,0 +1,6 @@
+class ResultData {
+  String? msg;
+  int? code;
+  var data;
+  ResultData(this.msg, this.code, this.data);
+}
